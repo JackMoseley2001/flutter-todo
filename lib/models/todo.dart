41e7c8415b59d7f26f6264f89fbb0e0d158ffef5
Jack Moseley <jackwmoseley@gmail.com>
2020-11-07 13:56:56 +0000

    Added uuid ID to todo class

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,6 +1,8 @@
 import 'package:meta/meta.dart';
+import 'package:uuid/uuid.dart';
 
 class Todo {
+  final String id = Uuid().v4();
   String title;
   String description;
   bool completed = false;
